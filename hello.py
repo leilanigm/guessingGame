f446597139477983d83554a7248ea5330ba39c83
Martinez <365736@mcpsmd.org>
2019-11-13 09:13:56 -0500

    Adds age range customization

--- hello.py
+++ hello.py
@@ -2,4 +2,11 @@ name = input("What's your name? ")
 age = int(input("What's your age? "))
 
 print("Hello " + name + "!")
-print("You are " + str(age) + "years old")
+print("You are " + str(age) + " years old")
+
+if age >= 13 and age <=19:
+    print("You are a teen")
+elif age < 13:
+    print("You are a child")
+else:
+    print("You are an adult")
